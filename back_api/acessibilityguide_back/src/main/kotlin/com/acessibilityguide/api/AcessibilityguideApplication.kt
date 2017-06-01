34e94bb720b520990499400700976d5287da312a
Eduardo Lopes <eduardo.lopes.es@gmail.com>
2017-06-01 16:38:00 -0300

    Adição do projeto de back-end e da documentação

---
+++ back_api/acessibilityguide_back/src/main/kotlin/com/acessibilityguide/api/AcessibilityguideApplication.kt
@@ -0,0 +1,11 @@
+package com.acessibilityguide.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AcessibilityguideApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AcessibilityguideApplication::class.java, *args)
+}
