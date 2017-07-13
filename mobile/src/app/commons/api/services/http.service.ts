7618684b0da1c5a9b3d2dd1efa9a8fdfd424e96d
Marcos Flávio <marcosflavio@trixlog.com>
2017-07-13 02:35:59 -0300

    Add reqquet opts args on htpp service, login path renamed

--- mobile/src/app/commons/api/services/http.service.ts
+++ mobile/src/app/commons/api/services/http.service.ts
@@ -6,7 +6,6 @@ import {Injectable} from '@angular/core';
 import {ROOT} from '../constants/server.constants';
 import {Observable} from "rxjs/Observable";
 import {AuthenticationService} from "./authentication.service";
-import {GuideUserModel} from "../../models/guideUser";
 
 @Injectable()
 export class HttpService {
@@ -18,17 +17,17 @@ export class HttpService {
     }
 
     get(url: string, options?: RequestOptionsArgs) : Observable<Response> {
-        //let requestOptionsArgs: RequestOptionsArgs = options ? options : {headers: new Headers({'Content-Type': 'application/json',
-          //      'Authorization': 'Basic ' + btoa(this.authenticationService.user.username + ":" + this.authenticationService.user.password)})};
+        let requestOptionsArgs: RequestOptionsArgs = options ? options : {headers: new Headers({'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(this.authenticationService.user.username + ":" + this.authenticationService.user.password)})};
         url = this.urlRoot + url;
-        return this.http.get(url, options);
+        return this.http.get(url, requestOptionsArgs);
     }
 
     post(url: string, body: any, options?: RequestOptionsArgs) : Observable<Response> {
-        //let requestOptionsArgs: RequestOptionsArgs = options ? options : {headers: new Headers({'Content-Type': 'application/json',
-          //      'Authorization': 'Basic ' + btoa(this.authenticationService.user.username + ":" + this.authenticationService.user.password)})};
+        let requestOptionsArgs: RequestOptionsArgs = options ? options : {headers: new Headers({'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(this.authenticationService.user.username + ":" + this.authenticationService.user.password)})};
         url = this.urlRoot + url;
-        return this.http.post(url, body, options);
+        return this.http.post(url, body, requestOptionsArgs);
     }
 
     put(url: string, body: any, options?: RequestOptionsArgs) : Observable<Response> {
